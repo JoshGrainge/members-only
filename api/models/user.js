bb80adc19a2b9ae3c1a9f7ab73a3bf3195fcf77b
JoshGrainge <josh.grainge@hotmail.com>
2023-05-08 15:10:46 -0400

    Created user model

---
+++ api/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+UserSchema.virtual('url').get(function () {
+  return `/user/${this._id}`;
+});
+
+UserSchema.virtual('full_name').get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
