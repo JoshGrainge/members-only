1642577539b35b0e5a2fb1afbab74f59c3d3cb87
JoshGrainge <josh.grainge@hotmail.com>
2023-05-09 10:32:10 -0400

    Connect to mongodb from environment variable

--- api/app.js
+++ api/app.js
@@ -10,6 +10,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Connect mongoose to mongodb
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URL
+
+main().catch(err => console.log(err));
+async function main(){
+  await mongoose.connect(mongoDB)
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
