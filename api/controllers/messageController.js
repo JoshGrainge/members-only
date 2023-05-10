166ab913720959ab90a298bd282f69f0cdab2d55
JoshGrainge <josh.grainge@hotmail.com>
2023-05-10 18:59:42 -0400

    Created new message function

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -2,6 +2,7 @@ const Message = require('../models/message');
 const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 
+const mongoose = require('mongoose');
 
 exports.allMessages = asyncHandler(async (req, res, next) => {
 	const allMessages = await Message.find().populate('user');
@@ -9,6 +10,18 @@ exports.allMessages = asyncHandler(async (req, res, next) => {
 	res.json(allMessages)
 });
 
+exports.newMessage = asyncHandler(async (req, res, next) => {
 
+	let newMessage = {
+		user: new mongoose.Types.ObjectId(req.body.user),
+		message: req.body.message,
+		timestamp: Date.now(),
+	};
+	const message = new Message(newMessage);
+
+	message.save();
+
+	res.json(message);
+});
 
 
