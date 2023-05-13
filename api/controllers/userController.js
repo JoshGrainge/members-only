bd40d3fff0d7e19a3ac89647e5c762f24f068aed
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 11:07:40 -0400

    Removed unused routes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,15 +1,3 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 const Message = require('../models/message');
-
-exports.users = asyncHandler(async (req, res, next) => {
-  const [allUsers, allMessages] = await Promise.all([
-    User.find(),
-    Message.find().populate({ path: 'users' }),
-  ]);
-
-  res.render('index', {
-    users: allUsers,
-    messages: allMessages,
-  });
-});
