fa9476236ed7a99d067ab118cc2bec977138f3ac
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 12:35:44 -0400

    Added signup function

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -24,6 +24,37 @@ exports.new_user = asyncHandler(async (req, res, next) => {
   res.json(user);
 });
 
+exports.signup = asyncHandler(async (req, res, next) => {
+  // Check if data matches critera (all fields are filled , passwords match)
+
+  const userExists = await User.findOne({ username: req.body.username });
+
+  // Check if username already exists
+  if (userExists) {
+    return res.status(400).send({
+      message: 'User already exists with that name',
+    });
+  }
+
+  // Check that password fields match
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.status(400).send({
+      message: 'Passwords do not match',
+    });
+  }
+
+  const user = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  await user.save();
+
+  res.sendStatus(200);
+});
+
 exports.login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ username: req.body.username });
 
