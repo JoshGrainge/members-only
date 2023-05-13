e72c7c0aa9baced670d30b0917c03c935032c664
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 11:11:26 -0400

    Added signup route

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,4 +6,7 @@ const userController = require('../controllers/userController');
 /* GET user detail */
 router.get('/:id', userController.user_detail);
 
+/* POST sign up information */
+router.post('/signup', userController.new_user);
+
 module.exports = router;
