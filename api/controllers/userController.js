7072dce11352cec46110e266b87aeac2b001e70a
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 11:08:23 -0400

    Added new user function

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,16 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 const Message = require('../models/message');
+
+exports.new_user = asyncHandler(async (req, res, next) => {
+  const user = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  await user.save();
+
+  res.json(user);
+});
