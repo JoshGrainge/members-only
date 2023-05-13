ac5801e64e32e725e84d0ce6f11795c5002917c0
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 11:12:00 -0400

    Added login route

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,4 +9,7 @@ router.get('/:id', userController.user_detail);
 /* POST sign up information */
 router.post('/signup', userController.new_user);
 
+/* POST log in information */
+router.post('/login', userController.login);
+
 module.exports = router;
