7d233c4833c4f0afbc5d5a2eae0b194d1c6490b3
JoshGrainge <josh.grainge@hotmail.com>
2023-05-13 11:08:50 -0400

    Added function to get specific user details

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,15 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 const Message = require('../models/message');
+const mongoose = require('mongoose');
+
+exports.user_detail = asyncHandler(async (req, res, next) => {
+  const userObjectId = new mongoose.Types.ObjectId(req.params._id);
+
+  const user = await User.findById(userObjectId);
+
+  res.json(user);
+});
 
 exports.new_user = asyncHandler(async (req, res, next) => {
   const user = new User({
